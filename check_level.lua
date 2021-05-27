2aabe2f318459d1f94da66a77b2545d9d9e6cd4f
Soranan Rungtanee <62444295+soranankiw1@users.noreply.github.com>
2021-05-27 20:25:44 +0700

    Add files via upload

--- check_level.lua
+++ check_level.lua
@@ -2,10 +2,16 @@ while true do
     wait(5)
         function level_check()
             local plrLvl = game.Players.LocalPlayer.Data.Level.Value
+            local plr = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
                 if plrLvl <= 15 then
                  print("Your are now level", plrLvl)
-                fireclickdetector(game:GetService("Workspace").Quest["Bandit Quest"].HumanoidRootPart.clickdetector)
-                
+                 fireclickdetector(game:GetService("Workspace").Quest["Bandit Quest"].ClickDetector)
+                 for i,v in pairs(game:GetService("Workspace").Lives:GetChildren()) do
+                    if v.Name == "Bandit LV. 1" then
+                        wait(1)
+                        game:GetService("Workspace").Lives["Bandit LV. 1"].HumanoidRootPart.CFrame = plr
+                    end
+                end
                 end
                 if plrLvl > 15 and plrLvl <= 25 then
                     print("Your are now level", plrLvl)

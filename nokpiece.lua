a7f27dd1e6d851d8843ebbdf9b13b1293db5a896
Soranan Rungtanee <62444295+soranankiw1@users.noreply.github.com>
2021-05-28 13:37:55 +0700

    Add files via upload

--- nokpiece.lua
+++ nokpiece.lua
@@ -5,4 +5,6 @@ print("finish loading, please wait....")
 local nokpiece = "6574375780"
 if game.PlaceId == nokpiece then
     
-else print("sorry you are on the wrong game, please join nok piece before run the script...")
+else game.Players.LocalPlayer:Kick(
+    "sorry you are on the wrong game, please join nok piece before run the script..."
+)

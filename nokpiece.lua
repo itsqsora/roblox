b62ec064aef2d09f5b932f6c3156ba3ddcaf5be7
Soranan Rungtanee <62444295+soranankiw1@users.noreply.github.com>
2021-05-29 23:46:20 +0700

    Add files via upload

--- nokpiece.lua
+++ nokpiece.lua
@@ -4,7 +4,10 @@ print("finish loading, please wait....")
 --gameid
 local nokpiece = "6574375780"
 if game.PlaceId == nokpiece then
-    
-else game.Players.LocalPlayer:Kick(
-    "sorry you are on the wrong game, please join nok piece before run the script..."
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/soranankiw1/roblox/main/check_level.lua", true))()
+end
+if game.PlaceId ~= nokpiece then
+    game.Players.LocalPlayer:Kick(
+        "sorry you are on the wrong game, please join nok piece before run the script..."
 )
+end
